0c0ca636efee3c5d9e261feeaffefa732faacac2
Sarah Graves <sjgraves142@gmail.com>
2016-11-29 17:00:54 -0500

    Added function to clip high res RGB image

--- scripts/functions.R
+++ scripts/functions.R
@@ -68,7 +68,7 @@ create_extent_around_point <- function(point_coordinates,buffer_size){
 # create_extent_around_point(point_coordinates,40)
 
 #### SAVE IMAGE OBJECT TO FILE ####
-save_raster_image <- function(multi_band_raster,save_folder,file_name,flatten=TRUE){
+save_raster_image <- function(multi_band_raster,save_folder,file_name,flatten=TRUE,img_res=1){
   
   # save tif raster
   writeRaster(multi_band_raster,
@@ -78,7 +78,8 @@ save_raster_image <- function(multi_band_raster,save_folder,file_name,flatten=TR
   if(flatten == TRUE){
     
     # calculate the number of pixels wide or tall - assume image is square
-    num_pix <- multi_band_raster@extent@xmax - multi_band_raster@extent@xmin
+    num_pix <- (multi_band_raster@extent@xmax - multi_band_raster@extent@xmin)/img_res
+    
     
     # write tiff file
     # need to use this and not writeRaster because image need to be flattened to RGB image
@@ -92,9 +93,26 @@ save_raster_image <- function(multi_band_raster,save_folder,file_name,flatten=TR
               to=paste(save_folder,file_name,"_flat",".tfw",sep=""))
     
 }
+
+} ## END FUNCTION
+
+#### CREATE CLIPPED IMAGE OF RGB CAMERA IMAGES FOR PLOT ####
+create_RGB_clip <- function(plot_coords,source_folder){
   
+  # use function in plyr package
+  plot_coords_round <- round_any(plot_coords,1000)
+  
+  # use these values to find file to import
+  f <- list.files(source_folder,
+                  pattern = paste0("*",plot_coords_round[1],"_",plot_coords_round[2],"_image.tif$"),
+                  full.names = T)
+  
+  # load raster
+  r <- stack(f)
+  
+  r_clip <- crop(r,plot_extent)
+  
+  return(r_clip)
   
 }
-
-
 
